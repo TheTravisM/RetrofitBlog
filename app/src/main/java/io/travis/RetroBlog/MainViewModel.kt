fbe580505af04bd60d9cf1c6950fb15cbd01f182
Travis <TravisEMikolay+GitKraken@Gmail.com>
2021-09-29 16:21:47 -0400

    First Update

---
+++ app/src/main/java/io/travis/RetroBlog/MainViewModel.kt
@@ -0,0 +1,11 @@
+package io.travis.RetroBlog
+
+import androidx.lifecycle.ViewModel
+
+private const val TAG = "MainViewModel"
+class MainViewModel : ViewModel() {
+
+    fun getPosts() {
+        // TODO fill this in
+    }
+}
